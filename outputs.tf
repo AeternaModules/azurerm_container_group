f52ba853db56986d3633fe254de1540d81f09a15
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,110 +1,105 @@
-output "container_groups" {
-  description = "All container_group resources"
-  value       = azurerm_container_group.container_groups
-  sensitive   = true
-}
 output "container_groups_container" {
-  description = "List of container values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.container]
+  description = "Map of container values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.container }
   sensitive   = true
 }
 output "container_groups_diagnostics" {
-  description = "List of diagnostics values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.diagnostics]
+  description = "Map of diagnostics values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.diagnostics }
   sensitive   = true
 }
 output "container_groups_dns_config" {
-  description = "List of dns_config values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.dns_config]
+  description = "Map of dns_config values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.dns_config }
 }
 output "container_groups_dns_name_label" {
-  description = "List of dns_name_label values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.dns_name_label]
+  description = "Map of dns_name_label values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.dns_name_label }
 }
 output "container_groups_dns_name_label_reuse_policy" {
-  description = "List of dns_name_label_reuse_policy values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.dns_name_label_reuse_policy]
+  description = "Map of dns_name_label_reuse_policy values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.dns_name_label_reuse_policy }
 }
 output "container_groups_exposed_port" {
-  description = "List of exposed_port values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.exposed_port]
+  description = "Map of exposed_port values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.exposed_port }
 }
 output "container_groups_fqdn" {
-  description = "List of fqdn values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.fqdn]
+  description = "Map of fqdn values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.fqdn }
 }
 output "container_groups_identity" {
-  description = "List of identity values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.identity]
+  description = "Map of identity values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.identity }
 }
 output "container_groups_image_registry_credential" {
-  description = "List of image_registry_credential values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.image_registry_credential]
+  description = "Map of image_registry_credential values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.image_registry_credential }
   sensitive   = true
 }
 output "container_groups_init_container" {
-  description = "List of init_container values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.init_container]
+  description = "Map of init_container values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.init_container }
   sensitive   = true
 }
 output "container_groups_ip_address" {
-  description = "List of ip_address values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.ip_address]
+  description = "Map of ip_address values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.ip_address }
 }
 output "container_groups_ip_address_type" {
-  description = "List of ip_address_type values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.ip_address_type]
+  description = "Map of ip_address_type values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.ip_address_type }
 }
 output "container_groups_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.key_vault_key_id }
 }
 output "container_groups_key_vault_user_assigned_identity_id" {
-  description = "List of key_vault_user_assigned_identity_id values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.key_vault_user_assigned_identity_id]
+  description = "Map of key_vault_user_assigned_identity_id values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.key_vault_user_assigned_identity_id }
 }
 output "container_groups_location" {
-  description = "List of location values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.location]
+  description = "Map of location values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.location }
 }
 output "container_groups_name" {
-  description = "List of name values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.name]
+  description = "Map of name values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.name }
 }
 output "container_groups_network_profile_id" {
-  description = "List of network_profile_id values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.network_profile_id]
+  description = "Map of network_profile_id values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.network_profile_id }
 }
 output "container_groups_os_type" {
-  description = "List of os_type values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.os_type]
+  description = "Map of os_type values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.os_type }
 }
 output "container_groups_priority" {
-  description = "List of priority values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.priority]
+  description = "Map of priority values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.priority }
 }
 output "container_groups_resource_group_name" {
-  description = "List of resource_group_name values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.resource_group_name }
 }
 output "container_groups_restart_policy" {
-  description = "List of restart_policy values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.restart_policy]
+  description = "Map of restart_policy values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.restart_policy }
 }
 output "container_groups_sku" {
-  description = "List of sku values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.sku]
+  description = "Map of sku values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.sku }
 }
 output "container_groups_subnet_ids" {
-  description = "List of subnet_ids values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.subnet_ids]
+  description = "Map of subnet_ids values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.subnet_ids }
 }
 output "container_groups_tags" {
-  description = "List of tags values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.tags]
+  description = "Map of tags values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.tags }
 }
 output "container_groups_zones" {
-  description = "List of zones values across all container_groups"
-  value       = [for k, v in azurerm_container_group.container_groups : v.zones]
+  description = "Map of zones values across all container_groups, keyed the same as var.container_groups"
+  value       = { for k, v in azurerm_container_group.container_groups : k => v.zones }
 }
 
